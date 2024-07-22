e4c638137ea2f15b4cbbef7a0a0ba943fced5f7b
Mikhail Belozerov <belozerov.mg@gmail.com>
2024-07-23 01:30:53 +0700

    Creating a Navbar for logged-in users

--- carRentalAngular/src/app/auth/services/storage/storage.service.ts
+++ carRentalAngular/src/app/auth/services/storage/storage.service.ts
@@ -45,4 +45,9 @@ export class StorageService {
     const role: string = this.getUserRole();
     return role == "CUSTOMER";
   }
+
+  static logout(): void {
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.removeItem(USER);
+  }
 }
