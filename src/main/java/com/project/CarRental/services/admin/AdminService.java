4c7eb1729177f50f869f215cc63226e26964f529
Mikhail Belozerov <belozerov.mg@gmail.com>
2024-07-23 22:47:53 +0700

    Created get all cars api in spring boot

--- src/main/java/com/project/CarRental/services/admin/AdminService.java
+++ src/main/java/com/project/CarRental/services/admin/AdminService.java
@@ -3,8 +3,11 @@ package com.project.CarRental.services.admin;
 import com.project.CarRental.dto.CarDto;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AdminService {
 
     boolean postCar(CarDto carDto) throws IOException;
+
+    List<CarDto> getAllCars();
 }
