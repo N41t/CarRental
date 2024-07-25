cc65b096f6593c9f8e5dafad66ccda215cd3b5de
Mikhail Belozerov <belozerov.mg@gmail.com>
2024-07-25 16:27:36 +0700

    Integrated get car by id with angular

---
+++ carRentalAngular/src/app/modules/customer/components/book-car/book-car.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-car',
+  templateUrl: './book-car.component.html',
+  styleUrl: './book-car.component.scss'
+})
+export class BookCarComponent {
+
+}
